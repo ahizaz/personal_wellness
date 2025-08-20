64427f426e01550c6053812e311d5a891450e76b
Izaz Ahmed <izaz3531ahmed@gmail.com>
2025-08-20 14:35:42 +0600

    ....

--- lib/feature/explore/controller/routine_controller.dart
+++ lib/feature/explore/controller/routine_controller.dart
@@ -111,12 +111,10 @@ class RoutineController extends GetxController {
     startDate.value = null;
     endDate.value = null;
     selectedOrder.value = 0;
-    selectedEveningOrder.value=0;
     selectedTimes.clear();
     instructionText.value = '';
     productName.value = '';
     instructionController.clear();
- 
 
     Get.back();
     final BottomNavcontroller navController = Get.find();
