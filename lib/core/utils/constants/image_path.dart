fe211408d7700e00ddefb190d6a62f54031d2ce5
Izaz Ahmed <izaz3531ahmed@gmail.com>
2025-08-20 11:12:37 +0600

    ...

--- lib/core/utils/constants/image_path.dart
+++ lib/core/utils/constants/image_path.dart
@@ -1,5 +1,5 @@
 class ImagePath {
-  static const String splashScreenPic = "assets/images/Splash.png";
+  static const String splashScreenPic = "assets/images/splash_screen_pic.png";
   static const String gettingStarted = "assets/images/getting_started.png";
   static const String accountBackground = "assets/images/account_background.png";
   static const String privacypic = "assets/images/privacy_pic.png";
