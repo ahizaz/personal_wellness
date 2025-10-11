4ba8ea42bd3e900534fdd259a3758d25ac27a289
ahizaz <izaz3531ahmed@gmail.com>
2025-10-11 09:34:33 +0600

    working

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,6 +23,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -49,4 +50,5 @@ flutter {
 dependencies {
     implementation("androidx.concurrent:concurrent-futures:1.2.0")
     implementation("androidx.concurrent:concurrent-futures-ktx:1.2.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
