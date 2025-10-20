2e54cd6562ed90b9b0369d0406c7d7cb9b49cb97
ahizaz <izaz3531ahmed@gmail.com>
2025-10-20 15:37:58 +0600

    processing

--- lib/core/urls/urls.dart
+++ lib/core/urls/urls.dart
@@ -1,6 +1,6 @@
 class Urls{
-  // static const String baseUrl = "http://10.10.12.25:5005/api/v1";
-  static const String baseUrl = "http://69.62.70.69:5005/api/v1";
+  static const String baseUrl = "http://10.10.12.25:5005/api/v1";
+  //static const String baseUrl = "http://69.62.70.69:5005/api/v1";
   
   static const String register ="$baseUrl/user/create-user";
   static const String forgetpassword = "$baseUrl/auth/forgot-password";
@@ -8,7 +8,7 @@ class Urls{
   static const String verifyemail = "$baseUrl/auth/verify-email";
   static const String googlesignin = "$baseUrl/auth/google-login";
   static const String getallskinconditon = "$baseUrl/skin-condition/get-all";
-  static const String imageurl = "http://69.62.70.69:5005";
+  static const String imageurl = "http://10.10.12.25:5005";
   static const String getallproduct = "$baseUrl/product/get-all";
   static const String getProductDetails = "$baseUrl/product/details"; // Added product details endpoint
   static const String changepassword = "$baseUrl/auth/change-password";
